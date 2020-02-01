f38c22713564b8bcf1adfbb5d36a351ccbd32f68
Serhii <chuhaystyr@gmail.com>
2020-02-02 00:01:02 +0200

    edit app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
@@ -12,11 +11,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
-import {
-  MatDialogModule,
-  MAT_DIALOG_DEFAULT_OPTIONS
-} from '@angular/material/dialog';
-
+import { MatDialogModule, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsListComponent } from './components/forms-list/forms-list.component';
@@ -52,9 +47,7 @@ import { EditFormComponent } from './components/forms-list/edit-form/edit-form.c
   providers: [
     { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } },
   ],
-  entryComponents: [
-    EditFormComponent
-  ],
+  entryComponents: [EditFormComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
